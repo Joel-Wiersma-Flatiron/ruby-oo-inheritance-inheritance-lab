012524e1333677597da19894fe487dfa0e959f94
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-20 17:30:36 -0700

    commit tree 85de10c645960986be5b2a65240c859d4a415ac6

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,9 @@
-class Student 
-
+class Student < User
+    attr_accessor :knowledge
+    def initialize
+        self.knowledge = []
+    end
+    def learn(string)
+        self.knowledge << string
+    end
 end
